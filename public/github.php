b063ebad97c203a53bc8b563af71a40ad022a2f2
susritourtales <80307181+susritourtales@users.noreply.github.com>
2021-04-26 14:01:54 +0530

    code modified

--- public/github.php
+++ public/github.php
@@ -1,5 +1,5 @@
 <?php //`git pull`; ?>
 <?php 
-$output = shell_exec('sudo git pull https://github.com/susritourtales/beta.git');
+$output = shell_exec('sudo git pull https://github.com/susritourtales/html.git');
 echo "<pre>$output</pre>";
 ?>

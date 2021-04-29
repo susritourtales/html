f82fe25254708dab6d0fff1fdd61001555fa0a5b
susritourtales <80307181+susritourtales@users.noreply.github.com>
2021-04-29 08:29:43 +0530

    Update AdminController.php

--- public/beta/module/Admin/src/Controller/AdminController.php
+++ public/beta/module/Admin/src/Controller/AdminController.php
@@ -5981,7 +5981,7 @@ class AdminController extends BaseController
         /* $fullPath = "/var/www/html/public/beta/logs/db-$mY.log";
         $this->emailSTTLogFiles($email, $subject, 'logs', array('type'=>'db'), $fullPath); */
         if($mailed)
-            echo "http logs mailed";
+            echo "http logs mailed - $mailed";
         else
             echo "unknown error";
         exit;

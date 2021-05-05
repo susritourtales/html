e049fbb1d98b04bf6aaa3fd6853cab2d4622b21f
susritourtales <80307181+susritourtales@users.noreply.github.com>
2021-05-05 10:14:13 +0530

    Update AdminController.php

--- module/Admin/src/Controller/AdminController.php
+++ module/Admin/src/Controller/AdminController.php
@@ -5994,7 +5994,7 @@ class AdminController extends BaseController
         $dmY = date("d-m-Y");
         $fullPath = "/var/www/html/logs/dblogs/sttdb_$dmY.sql";
         $mailed = $this->emailSTTLogFiles($email, $subject, 'logs', array('type'=>'db'), $fullPath);
-                
+        print_r(error_get_last());
         /* if($mailed)
             echo "db logs mailed - $mailed";
         else

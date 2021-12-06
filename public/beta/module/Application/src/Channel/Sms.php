4cc956832d53283769a628535779619d513ccffc
susritourtales <80307181+susritourtales@users.noreply.github.com>
2021-12-06 19:27:34 +0530

    Update Sms.php

--- public/beta/module/Application/src/Channel/Sms.php
+++ public/beta/module/Application/src/Channel/Sms.php
@@ -24,7 +24,6 @@ class Sms
             $apiKey = urlencode('LYD9m1xiwRQ-aUZo0QDyAW4qpZZhVgYjBVjXhSvsmw');
             $test = false;
             // Message details
-            $mobile = '917330781638';
             $numbers = array($mobile);
             $sender = urlencode('STTMSG');
             $numbers = implode(',', $numbers);

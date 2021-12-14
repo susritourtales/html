ac50dde82a512ab0c324c0b6d353c08ab0c84b94
susritourtales <susritourtales@gmail.com>
2021-12-14 13:57:22 +0530

    Update UserTable.php

--- public/beta/module/Admin/src/Model/UserTable.php
+++ public/beta/module/Admin/src/Model/UserTable.php
@@ -1141,6 +1141,7 @@ class UserTable extends  BaseTable
             $query = $sql->select()
                 ->columns(array("user_id","user_name","mobile","mobile_country_code","subscription_end_date", "is_promoter"))
                 ->from(array('u' => 'users'))
+                ->join(array('pd'=>'promoter_details'), 'pd.user_id=u.user_id',array("state_city" ,"permanent_addr",'bank_name', 'ifsc_code', 'bank_ac_no'),'left')
                 ->where($where)
                 ->limit($data['limit'])
                 ->offset($data['offset'])

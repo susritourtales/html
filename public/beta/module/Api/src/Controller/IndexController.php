75721ace568e674d843eacd242b79bd2e20ba2b3
susritourtales <susritourtales@gmail.com>
2021-12-14 16:40:44 +0530

    Update IndexController.php

--- public/beta/module/Api/src/Controller/IndexController.php
+++ public/beta/module/Api/src/Controller/IndexController.php
@@ -919,8 +919,8 @@ class IndexController extends BaseController{
          }
          $data['discount_percentage'] = 30;
          $profileUpdate=$this->userTable()->updateUser($data,array('user_id'=>$userId));
-         var_dump($data); exit;
-         //var_dump($refData); exit;
+         //var_dump($data); exit;
+         var_dump($refData); exit;
          $refUpdate['success'] = true;
          if($refData)
             if($refData['ref_id'] != null)

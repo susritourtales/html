6c73a47faa54aaee2c85c6a22f1a67657e775ddc
susritourtales <susritourtales@gmail.com>
2021-12-16 18:59:47 +0530

    Update IndexController.php

--- public/beta/module/Api/src/Controller/IndexController.php
+++ public/beta/module/Api/src/Controller/IndexController.php
@@ -1339,6 +1339,7 @@ class IndexController extends BaseController{
         }
         $where = array("user_id" => $request['user_id']);
         $data = array("terms_accepted" => $request['terms_accepted']);
+        var_dump($data);
         $res = $this->promoterDetailsTable()->addUpdatePromoterDetails($data, $where);
         if($res){
             return new JsonModel(array('success'=>true,'message'=>'success'));

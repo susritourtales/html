78005b3d00b0ac75485c3131cd3d16057c2c8ab4
susritourtales <susritourtales@gmail.com>
2022-03-11 21:31:47 +0530

    Update IndexController.php

--- module/Api/src/Controller/IndexController.php
+++ module/Api/src/Controller/IndexController.php
@@ -4750,9 +4750,16 @@ class IndexController extends BaseController{
     }
 
     public function checkMailAction(){
+        $stream_opts = [
+            "ssl" => [
+                "verify_peer"=>false,
+                "verify_peer_name"=>false,
+            ]
+        ];
         $bookingList['passwords']=array('pwd1','pwd2');
         $bookingId = 'test';
-        $html = file_get_contents($this->getBaseUrl() . '/application/booking-pdf?suid=0&bid=' . $bookingId, true);// - removed by Manjary to make local work - use on live
+        $html = file_get_contents($this->getBaseUrl() . '/application/booking-pdf?bid=' . $bookingId, false, stream_context_create($stream_opts));
+        //$html = file_get_contents($this->getBaseUrl() . '/application/booking-pdf?suid=0&bid=' . $bookingId, true);
         $mpdf = new mPDF(['tempDir' => getcwd()."/public/data/temp"]);
         $mpdf->SetDisplayMode("fullpage");
         $mpdf->WriteHTML($html);

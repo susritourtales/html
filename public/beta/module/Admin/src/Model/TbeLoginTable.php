807c4a5ae6b148c24457667e4596a27d7ef0aeff
susritourtales <susritourtales@gmail.com>
2022-11-16 11:40:01 +0530

    Update TbeLoginTable.php

--- public/beta/module/Admin/src/Model/TbeLoginTable.php
+++ public/beta/module/Admin/src/Model/TbeLoginTable.php
@@ -126,7 +126,9 @@ class TbeLoginTable extends BaseTable
                          print_r($user);
                             exit;*/
                     $decryptedPassword = $aes->decrypt($user[0]["pwd"], $user[0]["hash"]);
-                    print_r($decryptedPassword);exit;
+                    $user[0] = $decryptedPassword;
+                    return $user[0];
+                    //print_r($decryptedPassword);exit;
                     if ($decryptedPassword == $password) {
                         return $user[0];
                     } else {

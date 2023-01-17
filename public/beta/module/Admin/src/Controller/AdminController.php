e81b2d1b76ad2a9c137246a44c50dacc70be6b36
susritourtales <80307181+susritourtales@users.noreply.github.com>
2023-01-17 14:48:39 +0530

    Update AdminController.php

--- public/beta/module/Admin/src/Controller/AdminController.php
+++ public/beta/module/Admin/src/Controller/AdminController.php
@@ -5396,8 +5396,8 @@ class AdminController extends BaseController
                         $sed = date_create($sed_str); //date('Y-m-d', strtotime($sed_str))
                         $sds_ed = date('Y-m-d', strtotime($request['tdate'] . " + 12 days"));
                         $sds_ed_dt = date_create($sds_ed);
-                        //$diff = date_diff($sds_ed_dt, $sed);
-                        return new JsonModel(array('success'=>false,'message'=>"($sds_ed) - ($sed) = " ));
+                        $diff = date_diff($sds_ed_dt, $sed);
+                        return new JsonModel(array('success'=>false,'message'=>"($sds_ed) - ($sed_str) = " . strval($diff)));
 
                         if($sds_ed <= $sed){ // if sds end date is within exisitng/running subscription period
                             return new JsonModel(array('success'=>false,'message'=>'Tourist is already subscriber of STT'));

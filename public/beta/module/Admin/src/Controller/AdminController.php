82001c473003b4c7a0e5f5db9a30589de27669a4
susritourtales <80307181+susritourtales@users.noreply.github.com>
2023-01-19 15:37:38 +0530

    Update AdminController.php

--- public/beta/module/Admin/src/Controller/AdminController.php
+++ public/beta/module/Admin/src/Controller/AdminController.php
@@ -5430,7 +5430,7 @@ class AdminController extends BaseController
                         'upc'=> $request['upc'],
                         'created_at' => date("Y-m-d H:i:s"),
                         'updated_at' => date("Y-m-d H:i:s"));
-                    return new JsonModel(array('success'=>false,'message'=>$tbeResp));
+                    return new JsonModel(array('success'=>false,'message'=>$taId));
 
                     $response=$this->taSdsTable()->addTaSDS($data);
                    

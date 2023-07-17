9b950de42c4bc863296e9adf34076844302e5035
susritourtales <susritourtales@gmail.com>
2023-07-17 16:31:38 +0530

    Update test.php

--- public/beta/public/test.php
+++ public/beta/public/test.php
@@ -1,1 +1,15 @@
-<?php echo "hello"; ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="style.css" />
+  <title>Figma Prototype</title>
+</head>
+
+<body>
+<iframe style="border: 1px solid rgba(0, 0, 0, 0.1);" width="800" height="450" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FdsCac8fFxBgFVrpR0G4OMO%2FANDAPP%3Ftype%3Ddesign%26node-id%3D142-348%26t%3DeNGbo92l8xzWuJ3W-1%26scaling%3Dscale-down%26page-id%3D0%253A1%26starting-point-node-id%3D160%253A290%26mode%3Ddesign" allowfullscreen></iframe>
+</body>
+
+</html>

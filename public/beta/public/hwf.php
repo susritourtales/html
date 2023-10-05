6b59f9ae5ae39a8ca4a2781056462151501822c7
susritourtales <susritourtales@gmail.com>
2023-10-05 13:21:16 +0530

    Update hwf.php

--- public/beta/public/hwf.php
+++ public/beta/public/hwf.php
@@ -23,12 +23,6 @@
             height: 600px;
         }
     }
-    @media only screen and (max-width: 600px) {
-        iframe{
-            height: 450px;
-        }
-}
-    
 </style>
 
 </html>

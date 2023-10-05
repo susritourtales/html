43e11419110210e35fbf082b90f33486656bb404
susritourtales <susritourtales@gmail.com>
2023-10-05 12:15:24 +0530

    wireframes upload
    high level wire frame upload

--- public/beta/public/test.php
+++ public/beta/public/test.php
@@ -13,9 +13,8 @@
 </head>
 
 <body>
-  <div>Hello</div>
 <div>
-<!-- <iframe style="border: 1px solid rgba(0, 0, 0, 0.1);" width="100%" height="900" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FdsCac8fFxBgFVrpR0G4OMO%2FANDAPP%3Ftype%3Ddesign%26node-id%3D142-348%26t%3DeNGbo92l8xzWuJ3W-1%26scaling%3Dscale-down%26page-id%3D0%253A1%26starting-point-node-id%3D160%253A290%26mode%3Ddesign" allowfullscreen></iframe> -->
+<iframe style="border: 1px solid rgba(0, 0, 0, 0.1);" width="100%" height="900" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FdsCac8fFxBgFVrpR0G4OMO%2FANDAPP%3Ftype%3Ddesign%26node-id%3D142-348%26t%3DeNGbo92l8xzWuJ3W-1%26scaling%3Dscale-down%26page-id%3D0%253A1%26starting-point-node-id%3D160%253A290%26mode%3Ddesign" allowfullscreen></iframe>
 </body>
 </div>
 

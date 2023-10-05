2a68fcb09ca567f1594569d41389741e22eca56c
susritourtales <susritourtales@gmail.com>
2023-10-05 11:48:41 +0530

    Create hwf.php

---
+++ public/beta/public/hwf.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="style.css" />
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  <title>Figma Prototype</title>
+</head>
+
+<body>
+<div>
+<iframe style="border: 1px solid rgba(0, 0, 0, 0.1);" width="100%" height="900" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FdsCac8fFxBgFVrpR0G4OMO%2FANDAPP%3Ftype%3Ddesign%26node-id%3D142-348%26t%3DeNGbo92l8xzWuJ3W-1%26scaling%3Dscale-down%26page-id%3D0%253A1%26starting-point-node-id%3D160%253A290%26mode%3Ddesign" allowfullscreen></iframe>
+</body>
+</div>
+
+</html>

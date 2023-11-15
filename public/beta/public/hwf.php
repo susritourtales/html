4cb0b2dcc9746a2067057cf13b147e1676c25b84
susritourtales <susritourtales@gmail.com>
2023-11-15 10:16:03 +0530

    Update hwf.php
    update

--- public/beta/public/hwf.php
+++ public/beta/public/hwf.php
@@ -14,7 +14,8 @@
 
 <body>
 <div>
-<iframe style="border: 1px solid rgba(0, 0, 0, 0.1);" width="100%" height="900" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FdsCac8fFxBgFVrpR0G4OMO%2FANDAPP%3Ftype%3Ddesign%26node-id%3D1135-4279%26t%3D11pPNxpOsrbWEi6h-1%26scaling%3Dscale-down%26page-id%3D51%253A332%26starting-point-node-id%3D1130%253A7593%26mode%3Ddesign" allowfullscreen></iframe>
+<!-- <iframe style="border: 1px solid rgba(0, 0, 0, 0.1);" width="100%" height="900" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FdsCac8fFxBgFVrpR0G4OMO%2FANDAPP%3Ftype%3Ddesign%26node-id%3D1135-4279%26t%3D11pPNxpOsrbWEi6h-1%26scaling%3Dscale-down%26page-id%3D51%253A332%26starting-point-node-id%3D1130%253A7593%26mode%3Ddesign" allowfullscreen></iframe> -->
+  <iframe style="border: 1px solid rgba(0, 0, 0, 0.1);" width="100%" height="900" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FdsCac8fFxBgFVrpR0G4OMO%2FANDAPP%3Fpage-id%3D51%253A332%26type%3Ddesign%26node-id%3D1959-12261%26viewport%3D926%252C8%252C0.12%26t%3Dd6FwQ16bbUJAUtv8-1%26scaling%3Dscale-down%26starting-point-node-id%3D1959%253A9793%26mode%3Ddesign" allowfullscreen></iframe>
 </body>
 </div>
 </html>

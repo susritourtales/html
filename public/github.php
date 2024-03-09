e3ef45bdfe6691919966091fe611034e6e417a9a
susritourtales <80307181+susritourtales@users.noreply.github.com>
2024-03-09 19:20:37 +0530

    Update github.php

--- public/github.php
+++ public/github.php
@@ -1,6 +1,6 @@
 <?php
 $gitCommand = 'sudo /usr/bin/git pull https://github.com/susritourtales/html.git';
-$output = shell_exec($gitCommand . ' 2>&1');  // Capture both STDOUT and STDERR
+$output = shell_exec($gitCommand . ' 2>&1');  // to Capture both STDOUT and STDERR
 if ($output === null) {
   echo "Command execution failed or disabled in your server configuration.";
 } else {

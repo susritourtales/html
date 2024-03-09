472c2dad0532e2b0efe93dcf61b81fac442c5dd8
susritourtales <80307181+susritourtales@users.noreply.github.com>
2024-03-09 19:12:51 +0530

    Update github.php

--- public/github.php
+++ public/github.php
@@ -1,5 +1,4 @@
 <?php
-//$gitCommand = 'pwd';
 $gitCommand = 'sudo /usr/bin/git pull https://github.com/susritourtales/html.git';
 $output = shell_exec($gitCommand . ' 2>&1');  // Capture both STDOUT and STDERR
 if ($output === null) {

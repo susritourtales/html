80308056c4f87c43ea8a54d516d7816a4a74fff8
susritourtales <80307181+susritourtales@users.noreply.github.com>
2024-03-09 18:04:20 +0530

    github fix
    github fix

--- public/github.php
+++ public/github.php
@@ -1,9 +1,17 @@
-<?php //`git pull`; 
-?>
 <?php
-$output = shell_exec('sudo /var/www/html git pull https://github.com/susritourtales/html.git');
+$gitCommand = 'sudo /usr/bin/git pull https://github.com/susritourtales/html.git';
+$output = shell_exec($gitCommand . ' 2>&1');  // Capture both STDOUT and STDERR
+if ($output === null) {
+  echo "Command execution failed or disabled in your server configuration.";
+} else {
+  echo "<pre>$output</pre>";
+}
+?>
+
+<?php
+/* $output = shell_exec('sudo git pull https://github.com/susritourtales/html.git');
 if ($output === null)
   echo "unknown issue";
 else
-  echo "<pre>$output</pre>";
+  echo "<pre>$output</pre>"; */
 ?>

13a1d9ea63745e9bb23f99a6f2c24f8b81221637
susritourtales <80307181+susritourtales@users.noreply.github.com>
2024-03-10 02:42:52 +0530

    Update index.php

--- public/index.php
+++ public/index.php
@@ -46,7 +46,6 @@ $appConfig = require __DIR__ . '/../config/application.config.php';
 if (file_exists(__DIR__ . '/../config/development.config.php')) {
     $appConfig = ArrayUtils::merge($appConfig, require __DIR__ . '/../config/development.config.php');
 }
-var_dump($_SERVER);
-exit();
+
 // Run the application!
 Application::init($appConfig)->run();

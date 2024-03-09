1a48da638a7cfde7f668f7ab35eb2c1dd80ecb39
susritourtales <80307181+susritourtales@users.noreply.github.com>
2024-03-09 20:22:15 +0530

    Update module.config.php

--- module/Admin/config/module.config.php
+++ module/Admin/config/module.config.php
@@ -6,8 +6,6 @@ use Laminas\Router\Http\Segment;
 use Laminas\Router\Http\Literal;
 use Laminas\ServiceManager\AbstractFactory\ReflectionBasedAbstractFactory;
 
-echo "config file";
-exit();
 return [
     'router' => [
         'routes' => [

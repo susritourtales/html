dce55a80104200a8b6dc89a5738bb933c4b1d737
susritourtales <80307181+susritourtales@users.noreply.github.com>
2024-03-09 14:46:35 +0530

    github code
    github code updated

--- public/github.php
+++ public/github.php
@@ -1,5 +1,9 @@
-<?php //`git pull`; ?>
-<?php 
+<?php //`git pull`; 
+?>
+<?php
 $output = shell_exec('sudo git pull https://github.com/susritourtales/html.git');
-echo "<pre>$output</pre>";
+if (!$output)
+  echo "<pre>$output</pre>";
+else
+  echo "unknown issue";
 ?>

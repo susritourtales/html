8c9742b808c1dcb3cd9227e6ae113535946150f7
susritourtales <80307181+susritourtales@users.noreply.github.com>
2024-03-09 17:44:11 +0530

    git pull fix
    git pull fix

--- public/github.php
+++ public/github.php
@@ -1,9 +1,9 @@
 <?php //`git pull`; 
 ?>
 <?php
-$output = shell_exec('sudo git pull https://github.com/susritourtales/html.git');
-if (!$output)
-  echo "<pre>$output</pre>";
-else
+$output = shell_exec('sudo /var/www/html git pull https://github.com/susritourtales/html.git');
+if ($output === null)
   echo "unknown issue";
+else
+  echo "<pre>$output</pre>";
 ?>

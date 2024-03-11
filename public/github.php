1972dc89e36c33c6b6085ca3d9003e1e69431085
susritourtales <80307181+susritourtales@users.noreply.github.com>
2024-03-11 23:08:23 +0530

    Update github.php

--- public/github.php
+++ public/github.php
@@ -9,8 +9,11 @@ if ($output === null) {
 ?>
 
 <?php
+// Change the current working directory to the Git repository
+chdir('/var/www/html');
+
 // Execute a shell command and capture the last line of the output
-exec('sudo git pull https://github.com/susritourtales/html.git', $output, $returnCode);
+exec('git pull https://github.com/susritourtales/html.git', $output, $returnCode);
 
 // Display the output and return code
 echo "<pre>" . implode("\n", $output) . "</pre>";

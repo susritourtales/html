d23bd9723176275e843b16999ea13054bcb8ec48
susritourtales <susritourtales@gmail.com>
2024-03-22 13:48:46 +0530

    Update TourTalesTable.php

--- module/Admin/src/Model/TourTalesTable.php
+++ module/Admin/src/Model/TourTalesTable.php
@@ -158,6 +158,8 @@ class TourTalesTable extends  BaseTable
             }
             $resultSet = $sql->prepareStatementForSqlObject($query)->execute();
             if ($gc == 1){
+                var_dump($gc); exit;
+                var_dump(count($resultSet));exit;
                 return count($resultSet);
             }
             $countries = array();

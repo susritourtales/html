6caf9d95eb9eba59147917b58faa47a53e53483b
susritourtales <80307181+susritourtales@users.noreply.github.com>
2024-04-24 11:53:55 +0530

    Update edit-place.js

--- public/js/admin/edit-place.js
+++ public/js/admin/edit-place.js
@@ -169,6 +169,9 @@ $(document).ready(function ()
                                     "uploaded": true,
                                     'id': response.id
                                 };
+                                if(circle[fileID]) {
+                                    circle[fileID].animate(100);
+                                }
                                 if (uploadClicked) {
                                     var countryElement = $("#addPlace");
                                     countryElement.prop('disabled', false);

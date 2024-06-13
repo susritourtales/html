c202f4a74ec23e9e1d7129e589819de78aac5d96
susritourtales <susritourtales@gmail.com>
2024-06-13 13:04:57 +0530

    git ignore fix 2

---
+++ config/modules.config.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * List of enabled modules for this application.
+ *
+ * This should be an array of module namespaces used in the application.
+ */
+return [
+    'Laminas\Mail',
+    'Laminas\Session',
+    //'Laminas\Form',
+    'Laminas\Db',
+    'Laminas\Router',
+    'Laminas\Validator',
+    'Application',
+    'Admin',
+    'User',
+];

a380a588069040baadb9e31d2ae8e7139346bfcf
susritourtales <susritourtales@gmail.com>
2025-01-14 22:57:54 +0530

    Update hello.php

--- public/hello.php
+++ public/hello.php
@@ -1,5 +1,5 @@
 <?php
-echo "Hello";
+echo "Welcome";
 // echo $_SERVER['APPLICATION_ENV'];
 //echo "app_env- " . getenv('APPLICATION_ENV');
 //var_dump(php_ini_loaded_file(), php_ini_scanned_files());

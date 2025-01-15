318e8a19e752a37a56fde9d25d295c2b068f815a
susritourtales <susritourtales@gmail.com>
2025-01-15 13:59:35 +0530

    Update IndexController.php

--- module/User/src/Controller/IndexController.php
+++ module/User/src/Controller/IndexController.php
@@ -1150,7 +1150,7 @@ class IndexController extends BaseController
     // Apple Keys
     $client_id = 'com.susritourtales.twistt'; // Your Service ID
     $client_secret = $this->generateClientSecret();
-    $redirect_uri = 'https://www.susritourtales.com/';
+    $redirect_uri = 'myapp://callback'; //'https://www.susritourtales.com/twistt/app/auth';
 
     // Get the authorization code from the request
     $auth_code = $_POST['code'] ?? null;

6bf1a0dae46b2bf6d3a3e619821f1cb6975d6ef5
susritourtales <susritourtales@gmail.com>
2025-01-15 21:36:39 +0530

    Update IndexController.php

--- module/User/src/Controller/IndexController.php
+++ module/User/src/Controller/IndexController.php
@@ -1194,8 +1194,8 @@ class IndexController extends BaseController
       'refreshToken' => $data['refresh_token'] ?? null,
     ]);
     // $this->redirect()->toUrl($appUrl);
-    header("Location: $appUrl"); //exit;
-    return new ViewModel(['success' =>true,'appUrl' => $appUrl]);
+    header("Location: $appUrl"); exit;
+    //return new ViewModel(['success' =>true,'appUrl' => $appUrl]);
     // return new JsonModel(array('success'=>true,'data'=>$data));
   }
 

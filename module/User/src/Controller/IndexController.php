4b10122c6258cbefc52a1f13bcf519becdfceb7a
susritourtales <susritourtales@gmail.com>
2025-01-15 22:05:07 +0530

    Update IndexController.php

--- module/User/src/Controller/IndexController.php
+++ module/User/src/Controller/IndexController.php
@@ -1188,12 +1188,18 @@ class IndexController extends BaseController
 
     // Parse and return the tokens
     $data = json_decode($response, true);
-    $appUrl = "myapp://callback" . '?' . http_build_query([
+    // $appUrl = "myapp://callback" . '?' . http_build_query([
+    //   'authorizationCode' => $data['access_token'],
+    //   'idToken' => $data['id_token'],
+    //   'refreshToken' => $data['refresh_token'] ?? null,
+    // ]);
+    // $this->redirect()->toUrl($appUrl);
+
+    $appUrl = 'intent://callback?${'.http_build_query([
       'authorizationCode' => $data['access_token'],
       'idToken' => $data['id_token'],
       'refreshToken' => $data['refresh_token'] ?? null,
-    ]);
-    // $this->redirect()->toUrl($appUrl);
+    ]).'}#Intent;scheme=signinwithapple;end';
     header("Location: $appUrl"); exit;
     //return new ViewModel(['success' =>true,'appUrl' => $appUrl]);
     // return new JsonModel(array('success'=>true,'data'=>$data));
